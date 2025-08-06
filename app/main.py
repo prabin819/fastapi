dd2ea15636df72e07be13e88de8eeaeab23e36cf
prabin819 <manandharprabin819@gmail.com>
2025-08-06 16:03:18 +0545

    all about CORS

--- app/main.py
+++ app/main.py
@@ -4,10 +4,23 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine) 
+# we no longer need this since we are using alembic
+# this was the statement that was responsible for creating tables (using sqlalchemy)
 
 app = FastAPI()
+
+origins = ["https://www.google.com"]    #list of all the domains that will talk to our api
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # my_posts = [{"id": 1, "title": "title of post 1", "content": "this is content of post 1"},{"id": 2, "title": "title of post 2", "content": "this is content of post 2"}]
 
